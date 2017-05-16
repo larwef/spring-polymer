94d34d7a461060d9440a3ae944e2dae5472f799a
Lars Wefald <larwef@gmail.com>
2017-05-16 20:19:45 +0200

    Example: using thymeleaf to put request paramters into dom-module

--- src/main/java/springPolymer/Controller/IndexController.java
+++ src/main/java/springPolymer/Controller/IndexController.java
@@ -4,12 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index(Model model) {
+    public String index(@RequestParam(value="name", required=false, defaultValue="default") String name, Model model) {
+        model.addAttribute("name", name);
         return "index";
     }
 }
