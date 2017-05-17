8620aebab40862a09bf142f6937e94959c4703b3
Lars Wefald <larwef@gmail.com>
2017-05-17 13:30:51 +0200

    Example with request param and path variable

--- src/main/java/springPolymer/Controller/IndexController.java
+++ src/main/java/springPolymer/Controller/IndexController.java
@@ -2,6 +2,7 @@ package springPolymer.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,9 +10,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class IndexController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index(@RequestParam(value="name", required=false, defaultValue="default") String name, Model model) {
-        model.addAttribute("name", name);
+    @RequestMapping(value = "/*", method = RequestMethod.GET)
+    public String index(
+            @RequestParam(value="query", required=false, defaultValue="default") String query,
+            Model model){
+        model.addAttribute("query", query);
+        return "index";
+    }
+
+    @RequestMapping(value = "/{path}", method = RequestMethod.GET)
+    public String indexWithPath(
+            @PathVariable(value="path") String path,
+            @RequestParam(value="query", required=false, defaultValue="default") String query,
+            Model model){
+        model.addAttribute("query", query);
+        model.addAttribute("path", path);
         return "index";
     }
 }
